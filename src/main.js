5a1f3f28f1779d71c13d881e431119ebd08b722b
Katrina Pingryn <katep@indeed.com>
2024-02-09 17:09:05 -0600

    add changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,41 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
-const promise = welcome();
+// const question = "Will the weather be nice today?";
 
-console.log(promise);
+// const promise = welcome();
+// const promise = tell(question);
+
+// // console.log(promise);
+// // promise.then((response) => console.log(response));
+// // promise.then(console.log);
+// promise.then((fortune) => {
+//   console.log(question);
+//   console.log(fortune);
+// });
+
+// const question = "Will the weather be nice today?";
+// const tellPromise = tell(question);
+// tellPromise.then((fortune) => {
+//     console.log(question);
+//     console.log(fortune);
+//   });
+
+// const question = "Will the weather be nice today?";
+// const promise = tell(question);
+// promise
+//   .then((fortune) => {
+//     console.log(question);
+//     console.log(fortune);
+//   })
+//   .catch(console.log);
+
+  function getFortune(question) {
+    tell(question)
+    .then((fortune) => {
+      console.log("Your question was: " + question);
+      console.log(fortune);
+    })
+  }
+  let hi = getFortune("am I nice?");
+  console.log(hi)
+  
